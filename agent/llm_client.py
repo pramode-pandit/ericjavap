bb8a1730da566143b21a8bf2f4cea8adfb9ece4c
pramode-pandit <83385177+pramode-pandit@users.noreply.github.com>
2026-04-09 21:51:18 +0530

    Update llm_client.py

--- agent/llm_client.py
+++ agent/llm_client.py
@@ -4,6 +4,13 @@ import google.generativeai as genai
 # Configure API key
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 
+
+models = genai.list_models()
+
+for m in models:
+    print(m.name, m.supported_generation_methods)
+
+
 # Create model
 model = genai.GenerativeModel("gemini-1.5-flash-latest")
 
