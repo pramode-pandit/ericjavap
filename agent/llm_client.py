b627036c8224b510fa34fbdaf3a973b932f47022
pramode-pandit <83385177+pramode-pandit@users.noreply.github.com>
2026-04-09 21:59:42 +0530

    Update llm_client.py

--- agent/llm_client.py
+++ agent/llm_client.py
@@ -12,7 +12,7 @@ for m in models:
 
 
 # Create model
-model = genai.GenerativeModel("gemini-2.0-flash-lite")
+model = genai.GenerativeModel("gemini-pro-latest")
 
 def analyze_logs(logs):
     prompt = f"""
